4aefdda11671d1f1e7d8fa24903ce1e34217c9dc
Guille Bonafonte <guillem.bonafonte@isolutions.ch>
2023-04-05 21:43:49 +0200

    create playlist

--- backend/CreatePlaylist/__init__.py
+++ backend/CreatePlaylist/__init__.py
@@ -9,11 +9,20 @@ import os
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    scope = "user-library-read"
-    logging.info(os.environ["SPOTIPY_CLIENT_ID"])
-    logging.info("test")
+    scope = "user-library-read playlist-modify-public"
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = sp.current_user_saved_tracks()
+    tracks = []
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        tracks.append(track["id"])
+    user = sp.current_user()
+        
+    create_playlist = sp.user_playlist_create(user['id'], "Test playlist", public=True, collaborative=False, description='My api playlist')
+    sp.user_playlist_add_tracks(user['id'], create_playlist['id'], tracks, position=None)
+
     results = sp.current_user_saved_tracks()
     for idx, item in enumerate(results['items']):
         track = item['track']
